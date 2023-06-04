3802d0b2d6d0f47d4d795b105be9e56fe5ee4c63
AshishChaudhary3639 <ashishchaudhary3639@gmail>
2023-06-04 19:56:49 +0530

    added one more route to get sorted value with field name

--- routes/project.route.js
+++ routes/project.route.js
@@ -1,66 +1,82 @@
-const {Router} =require('express');
-const { authentication } = require('../middleware/authentication');
-const { ProjectModel } = require('../models/project.model');
+const { Router } = require("express");
+const { authentication } = require("../middleware/authentication");
+const { ProjectModel } = require("../models/project.model");
 
-const projectRoute=Router()
+const projectRoute = Router();
 
 // Project data storing into Database
-projectRoute.post('/createproject',authentication,async(req,res)=>{
-    let project=req.body;
-    try {
-        let data=new ProjectModel(project)
-        await data.save()
-        res.send({'success':'project add successfuly'})
-    } catch (error) {
-        res.send({'err':'something went wrong'})
-        
-    }
-})
+projectRoute.post("/createproject", authentication, async (req, res) => {
+  let project = req.body;
+  try {
+    let data = new ProjectModel(project);
+    await data.save();
+    res.send({ success: "project add successfuly" });
+  } catch (error) {
+    res.send({ err: "something went wrong" });
+  }
+});
 
-projectRoute.get('/getprojects',authentication,async(req,res)=>{
-    const page=req.query.page    
-    try {
-        let projects = await ProjectModel.find().skip(page*10).limit(10)
-        res.send(projects)
-    } catch (error) {
-        res.send({'err':'something went wrong'})
-        
-    }
-})
+projectRoute.get("/getprojects", authentication, async (req, res) => {
+  const { page } = req.query;
 
-projectRoute.patch('/getprojects/:id',authentication,async(req,res)=>{
-    const id=req.params.id
-    const valueToUpdate=req.body.status
-    const page=Number(req.body.page)
-    try {
-        await ProjectModel.findByIdAndUpdate(id,{"status":valueToUpdate})
-        let projects = await ProjectModel.find().skip(page*10).limit(10)
-        res.send(projects)
-    } catch (error) {
-        console.log(error)
-        res.send({'err':'something went wrong'})
-        
-    }
-})
+  try {
+    let projects = await ProjectModel.find()
+      .skip(page * 10)
+      .limit(10);
+    res.send(projects);
+  } catch (error) {
+    res.send({ err: "something went wrong" });
+  }
+});
+projectRoute.get("/getprojectsBySort", authentication, async (req, res) => {
+  const { page, sortVal } = req.query;
 
+  try {
+    let sortOptions = {};
+    sortOptions[sortVal] = 1;
+    let projects = await ProjectModel.find()
+      .sort(sortOptions)
+      .skip(page * 10)
+      .limit(10)
+      .exec();
+    res.send(projects);
+  } catch (error) {
+    res.send({ err: "something went wrong" });
+  }
+});
 
-projectRoute.get('/search',authentication,async(req,res)=>{
-    const val=req.query.query
-    try {
-        let projects = await ProjectModel.find({
-            $or:[
-                {"projectName":{ $regex: val, $options: "i" }},
-                {"type":{ $regex: val, $options: "i" }},
-                {"division":{ $regex: val, $options: "i" }},
-                {"category":{ $regex: val, $options: "i" }},
-                {"priority":{ $regex: val, $options: "i" }},
-                {"location":{ $regex: val, $options: "i" }},
-            ]
-        })
-        res.send(projects)
-    } catch (error) {
-        res.send({'err':'something went wrong'})
-        
-    }
-})
-module.exports={projectRoute}
+projectRoute.patch("/getprojects/:id", authentication, async (req, res) => {
+  const id = req.params.id;
+  const valueToUpdate = req.body.status;
+  const page = Number(req.body.page);
+  try {
+    await ProjectModel.findByIdAndUpdate(id, { status: valueToUpdate });
+    let projects = await ProjectModel.find()
+      .skip(page * 10)
+      .limit(10);
+    res.send(projects);
+  } catch (error) {
+    console.log(error);
+    res.send({ err: "something went wrong" });
+  }
+});
+
+projectRoute.get("/search", authentication, async (req, res) => {
+  const val = req.query.query;
+  try {
+    let projects = await ProjectModel.find({
+      $or: [
+        { projectName: { $regex: val, $options: "i" } },
+        { type: { $regex: val, $options: "i" } },
+        { division: { $regex: val, $options: "i" } },
+        { category: { $regex: val, $options: "i" } },
+        { priority: { $regex: val, $options: "i" } },
+        { location: { $regex: val, $options: "i" } },
+      ],
+    });
+    res.send(projects);
+  } catch (error) {
+    res.send({ err: "something went wrong" });
+  }
+});
+module.exports = { projectRoute };
